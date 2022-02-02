b53b1201cf3c98f013cbd9d9c846f82ead63890e
Sergitxin22 <65020729+Sergitxin22@users.noreply.github.com>
2022-02-02 21:44:23 +0100

    Create Cuadrado.java

---
+++ Reto4/Cuadrado.java
@@ -0,0 +1,13 @@
+package reto4;
+
+public class Cuadrado implements poligono{
+    private final double lado;
+
+   public final double getLado() {
+      return this.lado;
+   }
+
+   public Cuadrado(double lado) {
+      this.lado = lado;
+   }
+}
